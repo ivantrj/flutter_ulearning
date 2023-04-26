44680ece93677f27974f101f5a8af0951e03c258
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-04-26 14:29:14 +0200

    fix directinality

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_unnecessary_containers
 
-import 'package:flutter/cupertino.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,68 +16,113 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Scaffold(
-        body: Container(
-          margin: EdgeInsets.only(top: 34.h),
-          width: 375.w,
-          child: Stack(
-            children: [
-              PageView(
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: const Image(image: AssetImage("assets/images/reading.png")),
-                      ),
-                      Container(
-                        child: Text(
-                          "First see learning",
-                          style: TextStyle(color: Colors.black, fontSize: 24.sp, fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                        child: Text(
-                          "Forget about other apps",
-                          style: TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 14.sp, fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(15.w),
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: const Offset(0, 1),
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Next",
-                            style: TextStyle(color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.normal),
-                          ),
-                        ),
-                      ),
-                    ],
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          body: Container(
+            margin: EdgeInsets.only(top: 34.h),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                PageView(
+                  children: [
+                    _page(
+                      1,
+                      context,
+                      "Next",
+                      "First see learning",
+                      "Forget about other apps",
+                      "assets/images/reading.png",
+                    ),
+                    _page(
+                      2,
+                      context,
+                      "Next",
+                      "Connect with everyone",
+                      "Always keep in touch with your tutor!",
+                      "assets/images/man.png",
+                    ),
+                    _page(
+                      3,
+                      context,
+                      "Next",
+                      "Learn Anywhere",
+                      "Anytime, wnerever you want, wherever you are",
+                      "assets/images/boy.png",
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 100.h,
+                  child: DotsIndicator(
+                    dotsCount: 3,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    decorator: DotsDecorator(
+                      color: Colors.grey,
+                      activeColor: Colors.blue,
+                      size: const Size.square(8.0),
+                      activeSize: const Size(10.0, 8.0),
+                      activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                    ),
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _page(int index, BuildContext context, String buttonName, String title, String subTitle, String imagePath) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Image(image: AssetImage(imagePath)),
+        ),
+        Container(
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.black, fontSize: 24.sp, fontWeight: FontWeight.normal),
+          ),
+        ),
+        Container(
+          width: 375.w,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            subTitle,
+            style: TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 14.sp, fontWeight: FontWeight.normal),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+          width: 325.w,
+          height: 50.h,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.w),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              buttonName,
+              style: TextStyle(color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.normal),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
